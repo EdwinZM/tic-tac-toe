04b7e23de056745e1612110abb1b9995003864a6
Edwin Zwanenburg <edwinzwanenburg@Macs-MacBook-Pro.local>
2021-09-22 13:14:35 -0500

    added winner checking functionality

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ print("""
    |   |
 """)
 
-starting_player = input("Choose X or O to start!\n")
-
-column = int(input("Choose a column between 0 and 2\n"))
-row = int(input("Choose a row between 0 and 2\n"))
+starting_player = input("Choose X or O to start!\n").capitalize()
 
 tic_array = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 
-def play(col, row, player, array):
+def play(player, array):
     is_winner = False
 
-    array[col][row] = player
+    column = int(input("Choose a column between 0 and 2\n"))
+    row = int(input("Choose a row between 0 and 2\n"))
+
+    array[column][row] = player
 
     print(f"""
      {array[0][0]} | {array[1][0]}  | {array[2][0]}
@@ -28,16 +28,31 @@ def play(col, row, player, array):
      {array[0][2]} | {array[1][2]}  | {array[2][2]}
     """)
 
-    print("Time for the next player!")
 
-    if array[0][0] == player and array[0][1] == player and array[0][2] == player:
+    for column in array:
+        if column[0] == player and column[1] == player and column[2] == player:
+            is_winner = True
+            print(f"{player} won the game!")
+            return 
+
+    
+    if (array[0][0] == player and array[1][0] == player and array[2][0] == player) or (array[0][1] == player and array[1][1] == player and array[2][1] == player) or (array[0][2] == player and array[1][2] == player and array[2][2] == player) or (array[0][0] == player and array[1][1] == player and array[2][2] == player) or (array[0][2] == player and array[1][1] == player and array[2][0] == player):
         is_winner = True
-
-    if player == "X":
-        player = "O"
     else:
-        player = "X"
+        is_winner = False
+       
+    
+    if is_winner:
+        print(f"{player} won the game!")
+    else:
+        if player == "X":
+            player = "O"
+        else:
+            player = "X"
+        
+        print("Time for the next player!")
+        play(player, array)
     
     
 
-play(column, row, starting_player, tic_array)
+play(starting_player, tic_array)
